cb2d5ee8c6896ec4a3760b7638e94d976cd48938
santosh <santosh.ece06@gmail.com>
2021-06-01 12:51:24 +0530

    dpenedncey added.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,7 @@ import { Member3 } from "./views/Member3";
 
 // TEAM LEADER
 function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row" style={{ height: "100vh" }}>
-        <div className="col-12 col-md-6 bg-dark text-light d-flex align-items-center justify-content-center">
-          <Member1></Member1>
-        </div>
-        <div className="col-12 col-md-6 bg-secondary text-light d-flex align-items-center justify-content-center">
-          <Member2></Member2>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
